904271e5ee95b90f4bbebc99731fe9d4b1edc811
marthahab <marthahabtamu264@gmail.com>
2026-02-01 12:31:28 -0800

    rootset

--- Problem_28_Memory_Manager/src/RootSet.java
+++ Problem_28_Memory_Manager/src/RootSet.java
@@ -1,6 +1,34 @@
 package Problem_28_Memory_Manager.src;
+import java.util.HashSet;
+import java.util.Set;
 
 public class RootSet {
+
+
+    public class RootSet {
+
+        private Set<Integer> roots;
+
+        public RootSet() {
+            roots = new HashSet<>();
+        }
+
+        public void addRoot(int index) {
+            roots.add(index);
+        }
+
+        public void removeRoot(int index) {
+            roots.remove(index);
+        }
+
+        public Set<Integer> getRoots() {
+            return roots;
+        }
+
+        public void printRoots() {
+            System.out.println("Roots: " + roots);
+        }
+    }
 
     
 }
